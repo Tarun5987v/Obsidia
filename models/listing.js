9414d0b35cfc1845130d9670b40d2179102c2420
Tarun9891v <tarunsharmag4u@gmail.com>
2026-09-08 20:25:06 +0530

    figlet err fix

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
-const { default: string } = require("figlet/fonts/babyface-lame");
+// const { default: string } = require("figlet/fonts/babyface-lame");
 const listingSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String },
